e4710599f271b4da9466c32fb916f00f6b75210d
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2025-03-02 14:39:49 +0900

    refactor : member url 수정

--- src/main/java/primitive/community/app/domain/member/controller/MemberController.java
+++ src/main/java/primitive/community/app/domain/member/controller/MemberController.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
