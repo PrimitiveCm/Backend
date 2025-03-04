ff48db07e80234158ece07f117cea277d6ee988e
KwonMinwoo <qwerty029369@naver.com>
2025-03-04 22:03:42 +0900

    feat: 게시글 수정 구현

--- src/main/java/primitive/community/app/domain/post/controller/PostController.java
+++ src/main/java/primitive/community/app/domain/post/controller/PostController.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +38,10 @@ public class PostController {
     @GetMapping("/{postId}")
     public ResponseEntity<PostDto> getPost (@PathVariable Long postId) {
         return new ResponseEntity<>(postService.getPost(postId), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.updatePost(postDto), HttpStatus.OK);
     }
 }
--- src/main/java/primitive/community/app/domain/post/service/PostService.java
+++ src/main/java/primitive/community/app/domain/post/service/PostService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import primitive.community.app.domain.category.entity.Category;
 import primitive.community.app.domain.category.repository.CategoryRepository;
 import primitive.community.app.domain.member.entity.Member;
@@ -16,6 +17,7 @@ import primitive.community.app.security.principal.MemberPrincipal;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
     private final PostRepository postRepository;
     private final CategoryRepository categoryRepository;
@@ -29,13 +31,23 @@ public class PostService {
         return post;
     }
 
+    @Transactional(readOnly = true)
     public List<PostGetListDto> getPostList(Long categoryId) {
         List<PostGetListDto> foundPostList = postRepository.findByCategory_CategoryId(categoryId);
         return foundPostList;
     }
 
+    @Transactional(readOnly = true)
     public PostDto getPost(Long postId) {
         Post targetPost = postRepository.findById(postId).orElseThrow();
         return PostDto.of(targetPost);
+    }
+
+    public PostDto updatePost(PostDto postDto) {
+        Post targetPost = postRepository.findById(postDto.getPostId()).orElseThrow();
+        Category category = categoryRepository.findById(postDto.getCategoryId()).orElseThrow();
+
+        targetPost.updatePost(postDto.getTitle(), postDto.getContent(),category);
+        return postDto;
     }
 }
