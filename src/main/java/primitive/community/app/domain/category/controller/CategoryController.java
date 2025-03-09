f9784f1bcbcf7f897600091344737d4ec1eb4ae2
KwonMinwoo <qwerty029369@naver.com>
2025-03-09 18:35:18 +0900

    feat: 카테고리 생성 구현

--- src/main/java/primitive/community/app/domain/category/controller/CategoryController.java
+++ src/main/java/primitive/community/app/domain/category/controller/CategoryController.java
@@ -4,10 +4,10 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +24,11 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<CategoryDto>> getCategoryList() {
         return new ResponseEntity<>(categoryService.findCategoryList(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> createCategory(@RequestBody CategoryDto categoryDto) {
+        return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.OK);
     }
 
     @PutMapping
